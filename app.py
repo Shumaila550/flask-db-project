c8cad56d4b78e3795b67742efeb520418064b279
Shumaila550 <codecanvas78@gmail.com>
2026-02-11 16:50:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(BASE_DIR, 'database.db')
+database_url = os.getenv("DATABASE_URL")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+if database_url:
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-# ------------------ MODEL ------------------
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-
-# ------------------ ROUTES ------------------
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        name = request.form["name"]
-        email = request.form["email"]
-
-        new_user = User(name=name, email=email)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect(url_for("index"))
-
-    users = User.query.all()
-    return render_template("index.html", users=users)
-
-
-@app.route("/delete/<int:id>")
-def delete(id):
-    user = User.query.get_or_404(id)
-    db.session.delete(user)
-    db.session.commit()
-    return redirect(url_for("index"))
-
-
-@app.route("/edit/<int:id>", methods=["GET", "POST"])
-def edit(id):
-    user = User.query.get_or_404(id)
-
-    if request.method == "POST":
-        user.name = request.form["name"]
-        user.email = request.form["email"]
-        db.session.commit()
-        return redirect(url_for("index"))
-
-    return render_template("edit.html", user=user)
-
-
-# ------------------ RUN ------------------
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
